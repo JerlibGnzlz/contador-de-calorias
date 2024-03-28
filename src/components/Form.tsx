3f252ad397f7e4fafae102f69cb5cc8779ec1317
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-03-27 23:01:05 -0300

    validando formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,41 @@
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 import { categories } from "../data/category"
+import { actividad } from "../types"
 
 
 
 export const Form = () => {
 
-    const [actividad, setActividad] = useState({
+    const [actividad, setActividad] = useState<actividad>({
         categoria: 1,
         name: "",
         calorias: 0
     })
 
+    const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+
+        const isNumber = ["categoria", "calorias"].includes(e.target.id)
+
+        setActividad({
+            ...actividad,
+            [e.target.id]: isNumber ? +e.target.value : e.target.value,
+        })
+    }
+
+    const isValidActividad = () => {
+        const { name, calorias } = actividad
+        return name.trim() !== "" && calorias > 0
+
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
+
     return (
         <form
             className="space-y-5 bg-white shadow p-10 rounded-lg"
+            onSubmit={handleSubmit}
         >
             <div className="grid grid-cols-2 gap-3">
                 <label
@@ -24,6 +46,7 @@ export const Form = () => {
                     className="border border-slate-300  p-2 rounded-lg w-full bg-white"
                     name="" id="categoria"
                     value={actividad.categoria}
+                    onChange={handleChange}
                 >
                     {categories.map(categoria => (
                         <option
@@ -46,6 +69,7 @@ export const Form = () => {
                     type="text"
                     className="border border-slate-300 p-2 rounded-lg"
                     placeholder="Ej. Comida, Ensaladas, Ejercicios, Pesas, Bicicleta"
+                    onChange={handleChange}
                     value={actividad.name}
                 />
             </div>
@@ -61,13 +85,15 @@ export const Form = () => {
                     className="border border-slate-300 p-2 rounded-lg"
                     placeholder="Calorias ej. 300 o 500"
                     value={actividad.calorias}
+                    onChange={handleChange}
                 />
             </div>
 
             <input
                 type="submit"
-                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white text-center cursor-pointer"
-                value="Guardar Comida o guarda ejercicio"
+                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white text-center cursor-pointer disabled:opacity-40"
+                disabled={!isValidActividad()}
+                value={actividad.categoria === 1 ? "Guardar Comida" : "Guardar Ejercicio"}
             />
 
         </form>
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,10 @@ export type Categoria = {
     id: number,
     name: string
 }
+
+
+export type actividad = {
+    categoria: number,
+    name: string,
+    calorias: number
+}
