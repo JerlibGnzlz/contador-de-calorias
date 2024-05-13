92ef91f2e4e4a1dde1d767f13765436cee891833
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-05-13 19:19:44 -0300

    terminando el useReducer

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,19 +1,21 @@
-import { useMemo } from 'react';
-import { categories } from '../data/category';
-import { actividad } from '../types';
-import { PencilSquareIcon } from "@heroicons/react/24/outline"
-import { ActivityAction } from '../reducers/avtivity-reducers';
-
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { useMemo } from "react";
+import { categories } from "../data/category";
+import { actividad } from "../types";
+import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/24/outline";
+import { ActivityAction } from "../reducers/avtivity-reducers";
 
 type ActivityListProps = {
-  actividad: actividad[]
-  dispatch: React.Dispatch<ActivityAction>
-}
+  actividad: actividad[];
+  dispatch: React.Dispatch<ActivityAction>;
+};
 
 export const ActivityList = ({ actividad, dispatch }: ActivityListProps) => {
-
-  const categoryName = useMemo(() =>
-    (categoria: actividad["categoria"]) => categories.map(cat => cat.id === categoria ? cat.name : ""), [])
+  const categoryName = useMemo(
+    () => (categoria: actividad["categoria"]) =>
+      categories.map((cat) => (cat.id === categoria ? cat.name : "")),
+    []
+  );
 
   return (
     <>
@@ -21,36 +23,46 @@ export const ActivityList = ({ actividad, dispatch }: ActivityListProps) => {
         Comida y Actividades
       </h2>
 
-
-      {actividad.map(actividad => (
-
-        <div key={actividad.id} className='px-5 py-10 bg-white mt-5 flex justify-between'>
-          <div className='space-y-2 relative'>
-            <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${actividad.categoria == 1 ? "bg-lime-500" : "bg-orange-700"} `}>
-              {categoryName(+actividad.categoria)} </p>
-            <p className='text-2xl font-bold pt-5'>
-              {actividad.name}</p>
-            <p className='font-black text-4xl text-lime-500'>
-              {actividad.calorias}{" "}
-              <span>Calorias</span>
+      {actividad.map((actividad) => (
+        <div
+          key={actividad.id}
+          className="px-5 py-10 bg-white mt-5 flex justify-between">
+          <div className="space-y-2 relative">
+            <p
+              className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${
+                actividad.categoria == 1 ? "bg-lime-500" : "bg-orange-700"
+              } `}>
+              {categoryName(+actividad.categoria)}{" "}
+            </p>
+            <p className="text-2xl font-bold pt-5">{actividad.name}</p>
+            <p className="font-black text-4xl text-lime-500">
+              {actividad.calorias} <span>Calorias</span>
             </p>
           </div>
 
-          <div className='flex gap-5 items-center'>
+          <div className="flex gap-5 items-center">
+            <button
+              onClick={() =>
+                dispatch({
+                  type: "set=activeId",
+                  payload: { id: actividad.id },
+                })
+              }>
+              <PencilSquareIcon className="w-8 h-8 text-gray-800" />
+            </button>
 
             <button
-              onClick={() => dispatch({
-                type: "set=activeId", payload: { id: actividad.id }
-              })}
-            >
-              <PencilSquareIcon
-                className='w-8 h-8 text-gray-800'
-              />
+              onClick={() =>
+                dispatch({
+                  type: "delete-activity",
+                  payload: { id: actividad.id },
+                })
+              }>
+              <XCircleIcon className="w-8 h-8 text-red-500" />
             </button>
           </div>
-        </div >
-
+        </div>
       ))}
     </>
-  )
+  );
 };
--- src/reducers/avtivity-reducers.ts
+++ src/reducers/avtivity-reducers.ts
@@ -1,16 +1,18 @@
-import { actividad } from '../types/index';
+import { actividad } from "../types/index";
 
 export type ActivityAction =
-  { type: "save=activity"; payload: { newActivity: actividad } } |
-  { type: "set=activeId"; payload: { id: actividad["id"] } };
+  | { type: "save=activity"; payload: { newActivity: actividad } }
+  | { type: "set=activeId"; payload: { id: actividad["id"] } }
+  | { type: "delete-activity"; payload: { id: actividad["id"] } };
 
 export type ActivityState = {
   activities: actividad[];
-  activeId: actividad["id"]
+  activeId: actividad["id"];
 };
+
 export const initialState: ActivityState = {
   activities: [],
-  activeId: ""
+  activeId: "",
 };
 
 export const activityReducer = (
@@ -19,17 +21,34 @@ export const activityReducer = (
 ) => {
   if (action.type === "save=activity") {
     //este codigo maneja la logica para actualizar el state
+
+    let updateActvivities: actividad[] = [];
+    if (state.activeId) {
+      updateActvivities = state.activities.map((actividad) =>
+        actividad.id === state.activeId ? action.payload.newActivity : actividad
+      );
+    } else {
+      updateActvivities = [...state.activities, action.payload.newActivity];
+    }
     return {
       ...state,
-      activities: [...state.activities, action.payload.newActivity],
+      activities: updateActvivities,
+      activeId: "",
     };
   }
   if (action.type === "set=activeId") {
-
     return {
       ...state,
-      activeId: action.payload.id
-    }
+      activeId: action.payload.id,
+    };
   }
+
+  if (action.type === "delete-activity") {
+    return {
+		...state,
+		activities: state.activities.filter(actividad=> actividad.id !== action.payload.id)
+	};
+  }
+
   return state;
 };
